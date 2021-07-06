434ad57c98149a7a49aadf8921301075910240a4
ioriayaka <ioriayaka@hotmail.com>
2021-07-06 22:03:22 +0800

    add CRUD function

--- app.js
+++ app.js
@@ -1,35 +1,117 @@
 //require packages used in the project
 const express = require('express')
-//將 JSON 檔案載入 Express 中
-const restaurants = require('./restaurant.json')
+// 載入 mongoose
+const mongoose = require('mongoose') 
+//載入 Restaurant Models
+const Restaurant = require('./models/restaurant')
+// 引用 body-parser
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 //require express-handlebars
 const exphbs = require('express-handlebars')
-
+// 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/restaurantData', { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 //setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
-//routes setting
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+//index routes setting
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurants.results})
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.error(error))
 })
 //search route
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const searchResults = restaurants.results.filter(restaurant => {
-    return restaurant.name_en.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) ||
-      restaurant.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) ||
-      restaurant.category.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
-  })
-  res.render('index', { restaurants: searchResults, keyword: keyword })
+app.get('/restaurants/search', (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase()
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => {
+      if (keyword) {
+        restaurants = restaurants.filter((restaurant) =>
+          restaurant.name.toLowerCase().includes(keyword) ||
+          restaurant.category.includes(keyword))
+      }
+      if (restaurants.length === 0) {
+        const error = '很遺憾，沒有符合搜尋的結果。'
+        return res.render('index', { error })
+      }
+      res.render('index', { restaurants })
+    })
+    .catch((error) => console.error(error))
 })
-//show page route
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurants.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)  
-  res.render('show', { restaurant: restaurant })
+// create route
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  if (!name || !name_en || !category || !image || !location || !phone || !google_map || !rating || !description) {
+    return res.redirect('/restaurants/new')
+  }
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch((error) => console.error(error))
+})
+//detail route
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('detail', { restaurant }))
+    .catch(error => console.log(error))
+})
+//Edit route
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      console.log(restaurant.image)
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+// delete route
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 //start and listen on the Express server
 app.listen(port, () => {
